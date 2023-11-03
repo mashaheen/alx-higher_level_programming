6822a385cc03ee360d4ee0c05578ebdead7363c4
mashaheen <finalflash147@yahoo.com>
2023-11-03 03:12:53 +0200

    a

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    
+    n = len(sys.argv) - 1
+    if n == 0:
+        print("{:d}".format(n))
+    else:
+        i = 1
+        add = 0
+        while i <= n:
+            add += int(sys.argv[i])
+            i += 1
+        print("{:d}".format(add))
