2cf728fc2c43b5c8141e6442ddd98fcc4d0485d4
Shaheen <finalflash147@yahoo.com>
2023-11-08 16:49:07 +0200

    a

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if (len(list(a_dictionary)) == 0):
-        return None
-    score = 0
-    lead = ''
-    for x, y in a_dictionary.items():
-        if y >= score:
-            score = y
-            lead = x
-    return lead
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
