01773051c64b7acbada669ffef7491fc9741e954
Shaheen <finalflash147@yahoo.com>
2023-11-21 14:28:28 +0200

    square_1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Defining a class"""
+
+
+class Square:
+    """class representing a square"""
+
+    def __init__(self, size):
+        """Make an instance of a square.
+
+        Args:
+            size : square size.
+        """
+        self.__size = size
