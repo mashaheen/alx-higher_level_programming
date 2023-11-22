ae980ca741fcb4466a11943e33cf5c42b89aa5a0
mashaheen <finalflash147@yahoo.com>
2023-11-22 04:32:16 +0200

    3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""Defining a class"""
+
+
+class Square:
+    """class representing a square"""
+
+    def __init__(self, size):
+        """Make an instance of a square.
+
+        Args:
+            size : square size.
+        """
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
